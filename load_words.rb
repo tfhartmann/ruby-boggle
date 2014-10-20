a39e57fe1b1df0e5961a0590c55700622426ab20
Tim Hartmann <tfhartmann@gmail.com>
2014-10-20 17:14:28 -0400

    Load words ruby

---
+++ load_words.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'json'
+require 'httparty'
+
+
+
+
+class Load_words
+  include HTTParty
+end
+
+response = Load_words.get('http://www.puzzlers.org/pub/wordlists/ospd.txt')
+#puts response.body, response.code, response.message, response.headers.inspect
+
+
+# Put data into an array
+a_body = response.body.split("\n")
+
+a_body.each do | word |
+        puts "this is a word : #{word}"
+end
