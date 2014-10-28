a0955020d65d12d998cc4b6c2152b2168d1123a8
Tim Hartmann <tfhartmann@gmail.com>
2014-10-27 21:52:55 -0400

    Messing with dicts

--- boggle.rb
+++ boggle.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'optparse'
+require 'set'
+require 'pp'
 require_relative 'load_words'
 
 options = {}
@@ -10,7 +12,9 @@ options = {}
 response = Load_words.get('http://www.puzzlers.org/pub/wordlists/ospd.txt')
 
 # Put data into an array
-a_body = response.body.split("\n")
+a_body = response.body.split("\n").to_set
+
+d_body = Hash[a_body.map {|x| [x, x]}]
 
 optparse = OptionParser.new do |opts|
     opts.banner = "Usage: boggle.rb [options]"
@@ -24,17 +28,19 @@ optparse = OptionParser.new do |opts|
 end
 optparse.parse!
 
+@word = options[:string] 
 
-a_body.each do | word |
-        #puts "this is a word : #{word}"
-        if options[:string] == word 
-            puts "Test!"
-            puts "test2"
-        end
+#puts "this is a word : #{word}"
+if d_body.has_key?(@word)
+    puts "Test!"
 end
+
 #debug
+#puts pp(d_body)
 puts options
 puts options[:string]
+puts "from set"
+puts d_body["zosters"]
 puts ARGV
 
 # Binary search  (cut list in half) 
