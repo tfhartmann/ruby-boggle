4802e9fb99fb5a3dff12f7451cc1b3ac2ce5efa8
Tim Hartmann <tfhartmann@gmail.com>
2014-11-24 09:46:05 -0500

    Adding new homework

---
+++ words_prefix.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+# Write a program that returns whether or not a string of letters is the prefix of some word.
+# Examples:
+# "zeb" -> true
+# "zebras" -> true
+# "zerb" -> false
