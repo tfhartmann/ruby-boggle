7d7c2e96ffad82136f140ad6aaedcd78c5cdf759
Tim Hartmann <tfhartmann@gmail.com>
2015-01-15 22:04:39 -0500

    we have been lossing at pandemic the cure...

---
+++ dice.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+require 'pp'
+
+# Given dice with a particular sides
+# given a particular number of dice
+# what is the probability that rolling that many dice 
+# adds up to a number greater then some target
+# also.. why can't I spell???
+#
+black_die = [ 0, 3, 3, 3, 5, 6 ]
+
+# what are the odds if  I roll 3 of them for the sum to be more then or == 11
+
+win = 0
+notwin = 0
+
+# pain in the ass nested for loops
+for i in 0...black_die.length
+  for j in 0...black_die.length
+    for k in 0...black_die.length
+      if black_die[i]+black_die[j]+black_die[k] >= 11
+        win = win+1
+      else 
+        notwin = notwin+1
+      end
+    end
+  end
+end
+puts (100.0*win/(notwin+win)).round(2)
+
+# more flexable (and sexy) recursion (vavavoom)
+def win_percent( dicenumber, number2beat, facesofdie )
+  @win = 0
+  @notwin = 0
+  def recurse ( dicenumber, number2beat, facesofdie, sumsofar ) 
+    if dicenumber == 0
+      if sumsofar >= number2beat
+        @win = @win+1
+      else
+        @notwin = @notwin+1
+      end
+    else
+      for i in 0...facesofdie.length
+        recurse( dicenumber-1, number2beat, facesofdie, sumsofar+facesofdie[i] )
+      end
+    end
+  end
+  recurse( dicenumber, number2beat, facesofdie, 0 )
+  percent = (100.0*@win/(@notwin+@win)).round(2)
+  puts "#{percent}%"
+end
+
+win_percent(6, 11, black_die)
